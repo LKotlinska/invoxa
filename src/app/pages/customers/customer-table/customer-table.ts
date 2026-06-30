8eecae48d77076d82ee3bc8da425a72c765ef88a
Laura Kotlinska <laura.devhub@gmail.com>
2026-06-30 19:41:07 +0200

    Added table component for customers' data

--- src/app/pages/customers/customer-table/customer-table.ts
+++ src/app/pages/customers/customer-table/customer-table.ts
@@ -2,16 +2,30 @@ import { AsyncPipe } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CustomerTableData } from '../../../../types/types';
+import { CustomerService } from '../../../services/customer.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-customer-table',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatTableModule],
   templateUrl: './customer-table.html',
   styleUrl: './customer-table.scss',
 })
 export class CustomerTable implements OnInit {
   customers$!: Observable<CustomerTableData[]>;
-  displayedColumns = signal<(keyof CustomerTableData)[]>(['id', 'name', 'email']);
+  displayedColumns = signal<(keyof CustomerTableData)[]>([
+    'id',
+    'full_name',
+    'email',
+    'street_name',
+    'postal_code',
+    'city',
+    'country',
+  ]);
 
-  ngOnInit(): void {}
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit(): void {
+    this.customers$ = this.customerService.getCustomers();
+  }
 }
