7b93013af6d6b50fbac52d16e3bff4eafcb48f2f
Laura Kotlinska <laura.devhub@gmail.com>
2026-07-02 14:45:55 +0200

    Update supabase types and add new types for orders

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,4 @@
+import { Data } from '@angular/router';
 import { Database } from './supabase';
 
 export type Invoice = Database['public']['Tables']['invoices']['Row'];
@@ -11,3 +12,7 @@ export type Customer = Database['public']['Tables']['customers']['Row'];
 export type CustomerTableData = Omit<Customer, 'county' | 'created_at'>;
 
 export type Product = Database['public']['Tables']['products']['Row'];
+
+export type Order = Database['public']['Tables']['orders']['Row'];
+
+export type OrderTableData = Omit<Order, 'product_id'> & { product_name: string };
