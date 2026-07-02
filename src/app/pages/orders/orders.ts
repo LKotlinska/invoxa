454867dd7766aaf136d3b726618e6e85c500d5fa
Laura Kotlinska <laura.devhub@gmail.com>
2026-07-02 16:05:34 +0200

    Fetch orders from database and display in a table

--- src/app/pages/orders/orders.ts
+++ src/app/pages/orders/orders.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { OrderTableData } from '../../../types/types';
+import { OrderService } from '../../services/order.service';
+import { DataTable } from '../../shared/data-table/data-table';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [DataTable],
   templateUrl: './orders.html',
   styleUrl: './orders.scss',
 })
-export class Orders {}
+export class Orders {
+  orders = signal<OrderTableData[]>([]);
+  displayedColumns = <(keyof OrderTableData)[]>[
+    'id',
+    'invoice_id',
+    'product_name',
+    'price',
+    'quantity',
+  ];
+  orderService = inject(OrderService);
+
+  ngOnInit(): void {
+    this.orderService.getOrders().subscribe((orders) => {
+      this.orders.set(orders);
+    });
+  }
+}
