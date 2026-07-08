ffedd79a749de34aa82dad57cf4d20e9775f9cef
Laura Kotlinska <laura.devhub@gmail.com>
2026-07-08 10:44:40 +0200

    Add error handling for input

--- src/app/shared/form-fields/outlined-input/outlined-input.ts
+++ src/app/shared/form-fields/outlined-input/outlined-input.ts
@@ -1,12 +1,14 @@
-import { Component, input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, DestroyRef, inject, input, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-outlined-input',
-  imports: [MatFormFieldModule, MatIconModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule, ReactiveFormsModule, FormsModule],
   templateUrl: './outlined-input.html',
   styleUrl: './outlined-input.scss',
 })
